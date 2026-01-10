cbb9f9f99746a2c309d8f520732c4dbd7f0b098d
advanced-bencoding <benjamin.joseph212620@gmail.com>
2026-01-10 19:22:37 +0530

    initial commit: db setup + project structure

---
+++ Auth.Api/Data/AuthDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Auth.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Api.Data;
+
+public class AuthDbContext(DbContextOptions<AuthDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("users");
+
+            entity.HasKey(u => u.UserId);
+
+            entity
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            entity
+                .Property(u => u.UserId)
+                .HasColumnName("user_id");
+
+            entity
+                .Property(u => u.FirstName)
+                .HasColumnName("first_name")
+                .IsRequired();
+            entity
+                .Property(u => u.MiddleName)
+                .HasColumnName("middle_name");
+            entity
+                .Property(u => u.LastName)
+                .HasColumnName("last_name")
+                .IsRequired();
+            entity
+                .Property(u => u.Email)
+                .HasColumnName("email")
+                .IsRequired();
+            entity
+                .Property(u => u.PasswordHash)
+                .HasColumnName("password_hash")
+                .IsRequired();
+        });
+    }
+}
---
+++ Auth.Api/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Auth.Api.Models;
+
+public class User
+{
+    public Guid UserId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string? MiddleName { get; set; }
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+}
